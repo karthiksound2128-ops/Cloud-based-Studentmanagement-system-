a7a971eab9d2b0640c882be7b8d0999f4be9cef1
Vignesh <karthiksound2128@gmail.com>
2026-07-24 11:16:21 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+
+alert("Welcome to Cloud-Based Student Management System");
+
+};
+
+
+// Future Functions
+
+function addStudent(){
+
+alert("Student Added Successfully");
+
+}
+
+function markAttendance(){
+
+alert("Attendance Updated");
+
+}
+
+function addMarks(){
+
+alert("Marks Added Successfully");
+
+}
+
+function generateReport(){
+
+alert("Report Generated");
+
+}
